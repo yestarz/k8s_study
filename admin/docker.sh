0bd39cb800a0af286ee42d7536158d7583ac26e9
yangxin <xinxiny@aliyun.com>
2022-09-22 11:55:22 +0800

    补充入门学习到的一些文件

---
+++ admin/docker.sh
@@ -0,0 +1,2 @@
+# install docker
+sudo apt install -y docker.io
---
+++ 入门篇学习/primary-test/docker-nginx-run.sh
@@ -0,0 +1,1 @@
+docker run -d --rm --net=host -v /home/yangxin/k8s/primary-test/nginx-wp.conf:/etc/nginx/conf.d/default.conf nginx:alpine
